5c088202f018311c0d28726b209164c685757380
cruelangel723 <58207677+cruelangel723@users.noreply.github.com>
2019-11-26 09:45:15 +0100

    Corrections mineures

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int addition (int a , int b);
+
 int main()
 {
     printf("1 + 2 = %d\n", addition(1, 2));
     return 0;
 }
+
 int addition (int a , int b)
 {
     return a+b ;
